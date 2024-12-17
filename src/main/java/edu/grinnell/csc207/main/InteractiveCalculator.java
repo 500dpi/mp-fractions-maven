9cdf320ecae5dd74af71804775765c88bfc16029
Sara_Jaljaa <jaljaasa@grinnell.edu>
2024-12-16 21:29:44 -0600

    refactoring

--- src/main/java/edu/grinnell/csc207/main/InteractiveCalculator.java
+++ src/main/java/edu/grinnell/csc207/main/InteractiveCalculator.java
@@ -2,12 +2,12 @@ package edu.grinnell.csc207.main;
 
 import edu.grinnell.csc207.util.BFCalculator;
 import edu.grinnell.csc207.util.BFRegisterSet;
+import edu.grinnell.csc207.util.BigFraction;
+
 import java.util.Scanner;
 import java.io.PrintWriter;
 
 /**
- * InteractiveCalculator.java
- *
  * A calculator class that can perform simple operations.
  *
  * @author Sara Jaljaa
@@ -16,7 +16,7 @@ import java.io.PrintWriter;
 public class InteractiveCalculator {
 
   /**
-   * Prints a calculate value from command line arguments.
+   * Calculates single expressions.
    *
    * @param args
    *    Command line arguments containing operations,
@@ -25,29 +25,229 @@ public class InteractiveCalculator {
   public static void main(String[] args) {
     BFCalculator calculator = new BFCalculator();
     BFRegisterSet register = new BFRegisterSet();
+
     PrintWriter pen = new PrintWriter(System.out, true);
     Scanner input = new Scanner(System.in);
 
     pen.println("Enter an expression to evaluate: ");
+    pen.print("> ");
+    pen.flush();
 
-    String expression = input.nextLine();
+    String[] expression = (input.nextLine()).split(" ");
 
-    while (!expression.equals("QUIT")) {
-      if (expression == null || expression == " ") {
-        System.err.println("Error: Cannot evaluate non-fraction terms.");
-      } // if
-
-      String[] arguments = expression.split(" ");
-
-      if (!(QuickCalculator.allChecks(arguments, register, calculator))) {
-        System.err.println("Error: Invalid input.");
-      } // if
-      calculator.clear();
-      expression = input.nextLine();
+    while(!expression[0].equals("QUIT")) {
+      parseExpression(expression, calculator, register, pen);
+      pen.print("> ");
+      pen.flush();
+      expression = (input.nextLine()).split(" ");
     } // while
-    input.close();
     pen.close();
-    register.empty();
-    calculator.clear();
+    input.close();
   } // main(String[])
+
+  /**
+   * Parses a string and evaluates the expression if it is valid.
+   *
+   * @param exp
+   *    The expression to evaluate.
+   * @param calc
+   *    The calculator to evaluate the expression with.
+   * @param reg
+   *    The register that stores characters to evaluate in expressions.
+   * @param pen
+   *    The pen that prints the evaluated expression.
+   * @return
+   *    A boolean representing whether an expression is valid or not.
+   */
+  public static boolean parseExpression(String[] exp, BFCalculator calc,
+      BFRegisterSet reg, PrintWriter pen) {
+    String operator = null;
+    String fraction = null;
+    BigFraction frac = null;
+
+    // Check if "STORE" is the first argument
+    if (exp[0].equals("STORE") && isRegister(exp[1])) {
+      reg.store(exp[1].charAt(0), calc.get());
+      pen.println("STORED");
+      calc.clear();
+      return true;
+    } else {
+      calc.clear();
+    } // elif
+
+    // Check for invalid flags
+    if (!flags(exp, calc, reg)) {
+      return false;
+    } // if
+
+    // Check if a fraction or register are the first argument
+    if (isRegister(exp[0])) {
+      calc.add(reg.get(exp[0].charAt(0)));
+    } else if (isFraction(exp[0])) {
+      calc.add(new BigFraction(exp[0]));
+    } // elif
+
+    // Check for normal fraction/register distribution through an
+    // expression
+    for (int i = 1; i < exp.length - 1; i =+ 2) {
+      operator = exp[i];
+      fraction = exp[i + 1];
+
+      // Ensure the operator is a mathematical operator
+      if (!isOperator(operator)) {
+        return false;
+      } // if
+
+      // If the value frac is a fraction or register, evaluate
+      // it appropriately
+      if (isFraction(fraction)) {
+        frac = new BigFraction(fraction);
+        pen.println("Frac has been assigned.");
+      } else if (isRegister(fraction)) {
+        if (reg.get(fraction.charAt(0)) != null) {
+          frac = reg.get(fraction.charAt(0));
+          pen.println("Frac has been registered.");
+        } else {
+          pen.println("Error: Not a valid register.");
+          return false;
+        } // elif
+      } else {
+        pen.println("Error: Not a fraction.");
+        return false;
+      } // elif
+      evaluate(operator, frac, calc);
+      pen.println("Got here.");
+      pen.println(calc.get().toString());
+    } // for
+    pen.println(calc.get().toString());
+    return true;
+  } //parseExpression(String[], BFCalculator, BFRegisterSet, PrintWriter)
+
+  /**
+   * Maps a string of expression arguments to parse
+   * (for multiple expressions entered in calcluator).
+   *
+   * @param exp
+   *    The expressions to evaluate.
+   * @param calc
+   *    The calculator to evaluate expressions with.
+   * @param reg
+   *    The register that stores characters.
+   */
+  public static void map(String[] exp, BFCalculator calc, BFRegisterSet reg) {
+    // IMPLEMENT: map string arrays to parseExpression, then input to parseExpression() call
+  } // map(String[], BFCalculator, BFRegisterSet)
+
+  /**
+   * Checks edge cases in an expression.
+   *
+   * @param exp
+   *    The expression as a string.
+   * @param calc
+   *    The BFcalculator object.
+   * @param reg
+   *    The register set.
+   * @return
+   *    True or false if the expression is valid.
+   */
+  public static boolean flags(String[] exp, BFCalculator calc, BFRegisterSet reg) {
+    // Case: Parameter is not 3 or 1.
+    if (exp.length % 2 == 1) {
+      return true;
+    } // if
+
+    // Case: Argument is not a register, fraction, or operator
+    for (int i = 0; i < exp.length; i++) {
+      if (!(isRegister(exp[0]) || isFraction(exp[0]) || isOperator(exp[0]))) {
+        return false;
+      } // if
+    } // for
+
+    // Case: Operator is first argument
+    if (isOperator(exp[0])) {
+      return false;
+    } // if
+    return true;
+  } // flags(String[], BFCalculator, BFRegister)
+
+  /**
+   * Check if the string is a register character.
+   *
+   * @param reg
+   *    A string that represents one lowercase letter register char.
+   * @return
+   *    A boolean value.
+   */
+  public static boolean isRegister(String reg) {
+    return (reg.length() == 1 && (reg.charAt(0) >= 'a'
+            && reg.charAt(0) <= 'z'));
+  } // isRegister(String)
+
+  /**
+   * Check if the string is a math operator matching
+   * +, -, *, or /.
+   *
+   * @param arg
+   *    A string variable.
+   * @return
+   *    A boolean value.
+   */
+  public static boolean isOperator(String arg) {
+    return (arg.equals("+") || arg.equals("-")
+            || arg.equals("/") || arg.equals("*"));
+  } // isOperator(String)
+
+  /**
+   * Checks if the string is a fraction.
+   *
+   * @param arg
+   *    A string variable.
+   * @return
+   *    A boolean value.
+   */
+  public static boolean isFraction(String arg) {
+    for (int i = 0; i < arg.length(); i++) {
+      if ((i == 0 || i == arg.length() - 1) && arg.charAt(i) == '/') {
+        return false;
+      } // if
+
+      if (i != 0 && arg.charAt(i) == '-') {
+        return false;
+      } // if
+      
+      if ((arg.charAt(i) >= '0') && (arg.charAt(i) <= '9')) {
+        return true;
+      } // if
+    } // for
+    return false;
+  } // isFraction(String)
+
+  /**
+   * Evaluate a fraction expression.
+   *
+   * @param symbol
+   *    The math operator.
+   * @param frac
+   *    The fraction to evaluate.
+   * @param calculator
+   *    The BFcalculator that stores the last computed fraction.
+   */
+  public static void evaluate(String symbol, BigFraction frac, BFCalculator calculator) {
+    switch (symbol) {
+      case "+":
+        calculator.add(frac);
+        break;
+      case "-":
+        calculator.subtract(frac);
+        break;
+      case "*":
+        calculator.multiply(frac);
+        break;
+      case "/":
+        calculator.divide(frac);
+        break;
+      default:
+        break;
+    } // switch
+  } // evaluate(String[], BigFraction, BFCalculator)
 } // class InteractiveCalculator
--- src/main/java/edu/grinnell/csc207/main/QuickCalculator.java
+++ src/main/java/edu/grinnell/csc207/main/QuickCalculator.java
@@ -7,8 +7,6 @@ import java.util.Scanner;
 import java.io.PrintWriter;
 
 /**
- * QuickCalculator.java
- *
  * A calculator that takes multiple arguments as input.
  *
  * @author Sara Jaljaa
@@ -19,7 +17,9 @@ public class QuickCalculator {
   /**
    * Prints a calculated value from expressions entered in command line arguments.
    *
-   * @param args Command line arguments containing operations, fractions, and string commands.
+   * @param args
+   *    Command line arguments containing operations, fractions,
+   *    and string commands.
    */
   public static void main(String[] args) {
     BFCalculator calculator = new BFCalculator();
@@ -27,89 +27,41 @@ public class QuickCalculator {
     PrintWriter pen = new PrintWriter(System.out, true);
     Scanner input = new Scanner(System.in);
     String[] arguments = null;
+    char ch;
 
     pen.println("Enter an expression to evaluate: ");
 
     String expression = input.nextLine();
 
     while (!expression.equals("QUIT")) {
-      if (expression.length() == 1 && (expression.charAt(0) <= ' ')) {
-        System.err.println("Error: Invalid input.");
-      } else {
-        arguments = expression.split(" ");
-
-        if (!(allChecks(arguments, register, calculator))) {
-          System.err.println("Error: Invalid input.");
-        } // if
-        calculator.clear();
-      } // if
-      expression = input.nextLine();
-      pen.flush();
+      ;
     } // while
 
-    if (arguments[0].equals("STORE")) {
-      if (regTest(arguments[1])) {
-        register.store(arguments[1].charAt(0), calculator.get());
-      } // if
-    } // if
+    // while (!expression.equals("QUIT")) {
+    //   if (expression.length() == 1 && (expression.charAt(0) <= ' ')) {
+    //     System.err.printf("Error: Invalid input: %s.\n", expression);
+    //   } else {
+    //     arguments = expression.split(" ");
 
-    input.close();
-    register.empty();
-    calculator.clear();
+    //     if (!(checkAll(arguments, register, calculator))) {
+    //       System.err.printf("Error: Invalid input: %s.\n", expression);
+    //     } // if
+    //     calculator.clear();
+    //   } // elif
+    //   expression = input.nextLine();
+    //   pen.flush();
+    // } // while
+
+    // if (arguments[0].equals("STORE")) {
+    //   if (isRegister(arguments[1])) {
+    //     register.store(arguments[1].charAt(0), calculator.get());
+    //   } // if
+    // } // if
+
+    // input.close();
+    // register.empty();
+    // calculator.clear();
   } // main(String[])
-
-  /**
-   * Checks if the string is a register character.
-   *
-   * @param arg
-   *    A string variable.
-   * @return
-   *    A boolean value.
-   */
-  public static boolean regTest(String arg) {
-    char[] reg = arg.toCharArray();
-    if (arg.length() == 1 && (reg[0] >= 'a' && reg[0] <= 'z')) {
-      return true;
-    } // if
-    return false;
-  } // regTest(String)
-
-  /**
-   * Returns true or false if the string is a math symbol matching +, -, *, or /.
-   *
-   * @param arg
-   *    A string variable.
-   * @return
-   *    A boolean value.
-   */
-  public static boolean symTest(String arg) {
-    if (arg.equals("+") || arg.equals("-") || arg.equals("/") || arg.equals("*")) {
-      return true;
-    } // if
-    return false;
-  } // symTest(String)
-
-  /**
-   * Checks if the string is a fraction.
-   *
-   * @param arg
-   *    A string variable.
-   * @return
-   *    A boolean value.
-   */
-  public static boolean fracTest(String arg) {
-    char[] frac = arg.toCharArray();
-    for (int i = 0; i < frac.length; i++) {
-      if (frac[i] == '/' && (i == 0 || i == frac.length - 1)) {
-        return false;
-      } else if ((frac[i] > '0') && (frac[i] < '9')) {
-        return true;
-      } else if (frac[i] == '-' && i != 0) {
-        return false;
-      } // if
-    } // for
-    return false;
-  } // fracTest(String)
 
   /**
    * Checks edge-cases and whether there is a register, fraction, and/or symbol present
@@ -124,59 +76,51 @@ public class QuickCalculator {
    * @return
    *    A boolean value.
    */
-  public static boolean allChecks(String[] args, BFRegisterSet register, BFCalculator calculator) {
-    PrintWriter pen = new PrintWriter(System.out, true);
-    BigFraction frac;
+  // public static boolean checkAll(String[] args, BFRegisterSet register, BFCalculator calculator) {
+    // PrintWriter pen = new PrintWriter(System.out, true);
+    // BigFraction frac;
 
-    for (int i = 0; i < args.length; i++) {
-      if (args[i] == null) {
-        return false;
-      } // if
-    } // for
+  //   if (args == null) {
+  //     return false;
+  //   } // if
+    
+  //   // Check that there is the correct arguments for an expression
+  //   if (args.length % 2 != 1 && !(args[0].equals("STORE"))) {
+  //     return false;
+  //   } // if
 
-    if (!(fracTest(args[0]) || symTest(args[0]) || regTest(args[0]))) {
-      return false;
-    } else if (fracTest(args[0])) {
-      calculator.add(new BigFraction(args[0]));
-    } else if (regTest(args[0])) {
-      calculator.add(register.get(args[0].charAt(0)));
-    } // if
+  //   // Check if the argument is a register command
+  //   if (args[0].equals("STORE") && isRegister(args[1])) {
+  //     register.store(args[1].charAt(0), calculator.get());
+  //   }
 
-    if ((args[0].compareTo("STORE") == 0)) {
-      if (regTest(args[1])) {
-        register.store(args[1].charAt(0), calculator.get());
-      } // if
-    } // if
+  //   if (!(isFraction(args[0]) || isSymbol(args[0]) || isRegister(args[0]))) {
+  //     return false;
+  //   } else if (isFraction(args[0])) {
+  //     calculator.add(new BigFraction(args[0]));
+  //   } else if (isRegister(args[0])) {
+  //     calculator.add(register.get(args[0].charAt(0)));
+  //   } // if
 
-    for (int i = 1; i < args.length; i += 2) {
-      if (fracTest(args[i + 1])) {
-        frac = new BigFraction(args[i + 1]);
-      } else if (regTest(args[i + 1]) && register.get(args[i + 1].charAt(0)) != null) {
-        frac = register.get(args[i + 1].charAt(0));
-      } else {
-        return false;
-      } // if
+  //   if ((args[0].compareTo("STORE") == 0)) {
+  //     if (isRegister(args[1])) {
+  //       register.store(args[1].charAt(0), calculator.get());
+  //     } // if
+  //   } // if
 
-      // Checks for operations & executes the operation if found
-      switch (args[i]) {
-        case "+":
-          calculator.add(frac);
-          break;
-        case "-":
-          calculator.subtract(frac);
-          break;
-        case "*":
-          calculator.multiply(frac);
-          break;
-        case "/":
-          calculator.divide(frac);
-          break;
-        default:
-          break;
-      } // switch
-    } // for
+  //   for (int i = 1; i < args.length; i += 2) {
+  //     if (isFraction(args[i + 1])) {
+  //       frac = new BigFraction(args[i + 1]);
+  //     } else if (isRegister(args[i + 1]) && register.get(args[i + 1].charAt(0)) != null) {
+  //       frac = register.get(args[i + 1].charAt(0));
+  //     } else {
+  //       return false;
+  //     } // if
 
-    pen.println(calculator.get().toString());
-    return true;
-  } // allChecks(Srting[], BFRegisterSet, BFCalculator)
+  //     evaluate(args[i], frac, calculator);
+  //   } // for
+
+  //   pen.println(calculator.get().toString());
+  //   return true;
+  // } // allChecks(String[], BFRegisterSet, BFCalculator)
 } // class QuickCalculator
